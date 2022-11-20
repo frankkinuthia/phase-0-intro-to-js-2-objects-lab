617360617c740a02a99844a4ad7e73c6f4c06e24
+ <+>
2022-11-20 15:18:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const employee={
+    name:"Sam",
+  };
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+      var newEmployee ={...employee};
+      newEmployee["streetAddress"]="11 Broadway";
+      return newEmployee;
+    }
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+      employee["streetAddress"]="12 Broadway";
+      return employee;
+  }
+  function deleteFromEmployeeByKey(employee, key){
+      var newEmployee={...employee};
+      delete newEmployee["name"];
+      return newEmployee;
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, key){
+      delete employee["name"];
+      return employee;
+  }
